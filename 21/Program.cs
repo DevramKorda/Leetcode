ca3e0e79bce3ce51e32e07f1b25b2fb42c22c251
Музыка Марк <muzyka.m.a@gmail.com>
2023-08-21 22:44:30 +0300

    Task 21

--- 21/Program.cs
+++ 21/Program.cs
@@ -9,25 +9,38 @@ namespace _21
             var list13 = new ListNode(4);
             var list12 = new ListNode(2, list13);
             var list1 = new ListNode(1, list12);
+            
+            var list23 = new ListNode(4);
+            var list22 = new ListNode(3, list23);
+            var list2 = new ListNode(1, list22);
 
-            var merge = MergeTwoLists(list1);
+            var merge = MergeTwoLists(list1, list2);
             var t = 0;
         }
         
-        public static ListNode MergeTwoLists(ListNode list1) //, ListNode list2
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-            ListNode res = new ListNode();
-            ListNode cur = list1;
-            while (true)
+            if (list1 == null && list2 == null)
             {
-                res.val = cur.val;
-                cur = cur.next;
-                res.next = cur;
-                if (cur == null)
-                {
-                    return res;
-                }
+                return null;
             }
+            
+            if (list1 == null && list2 != null)
+            {
+                return list2;
+            }
+            
+            if (list2 == null && list1 != null)
+            {
+                return list1;
+            }
+
+            if (list1.val <= list2.val)
+            {
+                return new ListNode(list1.val, MergeTwoLists(list1.next, list2));
+            }
+            
+            return new ListNode(list2.val, MergeTwoLists(list2.next, list1));
         }
         
         public class ListNode
